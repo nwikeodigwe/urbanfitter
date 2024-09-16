f9d374f7bffc917e0333ada7a20162e174d4c88a
Nwike Odigwe <nwikeodigwe@outlook.com>
2024-09-16 12:29:12 +0100

    Added helmet and compression middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,10 @@ const logger = require("./startup/logging");
 const app = express();
 
 require("./startup/routes")(app);
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+module.exports = app.listen(port, () => {
   logger.info(`Listening on port ${port}...`);
 });
